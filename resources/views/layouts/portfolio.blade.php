6218ad0a03ef8e4c6d9498eb39f25397abb43ced
Danijela <danijela.frontend@gmail.com>
2019-04-16 23:00:59 +0200

    Portfolio section

--- resources/views/layouts/portfolio.blade.php
+++ resources/views/layouts/portfolio.blade.php
@@ -1,28 +1,23 @@
 <section id="portfolio" class="portfolio-area">
+        
             <div class="container">
                 <h2 class="block_title">My Works</h2>
+                     <div class="row port cs-style-3">
+               
+                    <!-- Projects Menu -->
+                   
+                        <div class="portfolio-menu text-center">
+                            <button class="btn active" data-filter="*">All</button>
+                            <button class="btn" data-filter=".human">Human</button>
+                            <button class="btn" data-filter=".nature">Nature</button>
+                            <button class="btn" data-filter=".country">Country</button>
+                            <button class="btn" data-filter=".video">Video</button>
+                        </div>
+              
+              
+            </div>
                 <div class="row port cs-style-3">
-                    <div class="col-md-4 col-sm-6 col-xs-12 item-space">
-                        <figure>
-                            <img src="images/portfolio1.jpg" alt="img04">
-                            <figcaption>
-                                <h3>Settings</h3>
-                                <span>Jacob Cummings</span>
-                                <a href="#" class="button" >Take a look</a>
-                            </figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-md-4 col-sm-6 col-xs-12 item-space">
-                        <figure>
-                            <img src="images/portfolio2.jpg" alt="img01">
-                            <figcaption>
-                                <h3>Camera</h3>
-                                <span>Jacob Cummings</span>
-                                <a href="#" class="button" >Take a look</a>
-                            </figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-md-4 col-sm-6 col-xs-12 item-space">
+                     <div class="col-md-3 col-sm-6 col-xs-12 item-space">
                         <figure>
                             <img src="images/portfolio3.jpg" alt="img02">
                             <figcaption>
@@ -32,7 +27,7 @@
                             </figcaption>
                         </figure>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-12 item-space">
+                    <div class="col-md-3 col-sm-6 col-xs-12 item-space">
                         <figure>
                             <img src="images/portfolio4.jpg" alt="img04">
                             <figcaption>
@@ -42,7 +37,7 @@
                             </figcaption>
                         </figure>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-12 item-space">
+                    <div class="col-md-3 col-sm-6 col-xs-12 item-space">
                         <figure>
                             <img src="images/portfolio5.jpg" alt="img01">
                             <figcaption>
@@ -52,7 +47,7 @@
                             </figcaption> 
                         </figure>
                     </div>
-                    <div class="col-md-4 col-sm-6 col-xs-12 item-space">
+                    <div class="col-md-3 col-sm-6 col-xs-12 item-space">
                         <figure>
                             <img src="images/portfolio6.jpg" alt="img02">
                             <figcaption>
@@ -62,6 +57,49 @@
                             </figcaption>
                         </figure>
                     </div>
+                </div>
+                    <div class="row port cs-style-3">
+                    <div class="col-md-3 col-sm-6 col-xs-12 item-space">
+                        <figure>
+                            <img src="images/portfolio3.jpg" alt="img02">
+                            <figcaption>
+                                <h3>Music</h3>
+                                <span>Jacob Cummings</span>
+                                <a href="#" class="button" >Take a look</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 item-space">
+                        <figure>
+                            <img src="images/portfolio4.jpg" alt="img04">
+                            <figcaption>
+                                <h3>Settings</h3>
+                                <span>Jacob Cummings</span>
+                                <a href="#" class="button" >Take a look</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 item-space">
+                        <figure>
+                            <img src="images/portfolio5.jpg" alt="img01">
+                            <figcaption>
+                                <h3>Camera</h3>
+                                <span>Jacob Cummings</span>
+                                <a href="#" class="button" >Take a look</a>
+                            </figcaption> 
+                        </figure>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-xs-12 item-space">
+                        <figure>
+                            <img src="images/portfolio6.jpg" alt="img02">
+                            <figcaption>
+                                <h3>Music</h3>
+                                <span>Jacob Cummings</span>
+                                <a href="#" class="button" >Take a look</a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    
                     <div class="col-xs-12">
                         <div class="btn-center"><a href="#" class="big button">View all</a></div>
                     </div>
